8e3db16d3005a009e577d75735e8509df779202c
Alex Dereu <adereu08@gmail.com>
2022-05-31 15:42:27 +0200

    modif seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,33 +38,33 @@ user_patrick.avatar.attach(io: patrick, filename: "patrick")
 user_patrick.save!
 
 #### guilhem event creation
-event_guilhem_01 = Event.new({ title: "TGV Marseille Paris", venue: "Gare Saint-Charles", starts_at: DateTime.new(2022, 06, 05, 11, 30), address: "Avenue Pierre Semard, 13001 Marseille", latitude: "43.3029782", longitude: "5.3845614", category: "Voyage", identity_card: "Y", driver_card: "N", passport: "Y", user: user_guilhem })
+event_guilhem_01 = Event.new({ title: "TGV Marseille Paris", venue: "Gare Saint-Charles", starts_at: DateTime.new(2022, 06, 05, 11, 30), address: "Avenue Pierre Semard, 13001 Marseille", latitude: "43.3029782", longitude: "5.3845614", category: "Voyage", identity_card: true, passport: true, user: user_guilhem })
 event_guilhem_01.document.attach(io: billet1, filename: "TGV1")
 event_guilhem_01.save!
-event_guilhem_02 = Event.new({ title: "Match PSG OM", venue: "Parc des princes", starts_at: DateTime.new(2022, 06, 05, 20, 30), address: "24 Rue du Commandant Guilbaud, 75016 Paris", latitude: "48.840248107910156", longitude: "2.2515957355499268", category: "Sport", identity_card: "Y", driver_card: "N", passport: "Y", user: user_guilhem })
+event_guilhem_02 = Event.new({ title: "Match PSG OM", venue: "Parc des princes", starts_at: DateTime.new(2022, 06, 05, 20, 30), address: "24 Rue du Commandant Guilbaud, 75016 Paris", latitude: "48.840248107910156", longitude: "2.2515957355499268", category: "Sport", identity_card: true, passport: true, user: user_guilhem })
 event_guilhem_02.document.attach(io: billet2, filename: "PSG_OM")
 event_guilhem_02.save!
-event_guilhem_03 = Event.new({ title: "TGV Paris Marseille", venue: "Gare de Lyon", starts_at: DateTime.new(2022, 06, 06, 10, 00), address: "Place Louis-Armand, 75012 Paris", latitude: "48.8451154", longitude: "2.3730357", category: "Voyage", identity_card: "Y", driver_card: "N", passport: "Y", user: user_guilhem })
+event_guilhem_03 = Event.new({ title: "TGV Paris Marseille", venue: "Gare de Lyon", starts_at: DateTime.new(2022, 06, 06, 10, 00), address: "Place Louis-Armand, 75012 Paris", latitude: "48.8451154", longitude: "2.3730357", category: "Voyage", identity_card: true, passport: true, user: user_guilhem })
 event_guilhem_03.document.attach(io: billet3, filename: "TGV2")
 event_guilhem_03.save!
 
 #### alex event creation
-event_alex_01 = Event.new({ title: "Salon du Bourget", venue: "Aeroport du Bourget", starts_at: DateTime.new(2022, 06, 20, 10, 00), address: "24 Rue du Commandant Guilbaud, 75016 Paris", latitude: "48.840248107910156", longitude: "2.2515957355499268", category: "Autres", identity_card: "Y", driver_card: "N", passport: "Y", user: user_alex })
+event_alex_01 = Event.new({ title: "Salon du Bourget", venue: "Aeroport du Bourget", starts_at: DateTime.new(2022, 06, 20, 10, 00), address: "24 Rue du Commandant Guilbaud, 75016 Paris", latitude: "48.840248107910156", longitude: "2.2515957355499268", category: "Autres", identity_card: true, passport: true, user: user_alex })
 event_alex_01.document.attach(io: billet4, filename: "bourget")
 event_alex_01.save!
 
 #### rayane event creation
-event_rayane_01 = Event.new({ title: "Demo day", venue: "Le Wagon", starts_at: DateTime.new(2022, 06, 03, 17, 00), address: "16 Vla Gaudelet, 75011 Paris", latitude: "48.8648601", longitude: "2.3798866", category: "Autres", identity_card: "Y", driver_card: "N", passport: "Y", user: user_rayane })
+event_rayane_01 = Event.new({ title: "Demo day", venue: "Le Wagon", starts_at: DateTime.new(2022, 06, 03, 17, 00), address: "16 Vla Gaudelet, 75011 Paris", latitude: "48.8648601", longitude: "2.3798866", category: "Autres", identity_card: true, passport: true, user: user_rayane })
 event_rayane_01.document.attach(io: billet5, filename: "demo_day_1")
 event_rayane_01.save!
-event_rayane_02 = Event.new({ title: "Demo day", venue: "Le Wagon", starts_at: DateTime.new(2022, 06, 8, 17, 00), address: "16 Vla Gaudelet, 75011 Paris", latitude: "48.8648601", longitude: "2.3798866", category: "Autres", identity_card: "Y", driver_card: "N", passport: "Y", user: user_rayane })
+event_rayane_02 = Event.new({ title: "Demo day", venue: "Le Wagon", starts_at: DateTime.new(2022, 06, 8, 17, 00), address: "16 Vla Gaudelet, 75011 Paris", latitude: "48.8648601", longitude: "2.3798866", category: "Autres", identity_card: true, passport: true, user: user_rayane })
 event_rayane_02.document.attach(io: billet6, filename: "demo_day_2")
 event_rayane_02.save!
-event_rayane_03 = Event.new({ title: "Demo day", venue: "Le Wagon", starts_at: DateTime.new(2022, 06, 10, 17, 00), address: "16 Vla Gaudelet, 75011 Paris", latitude: "48.8648601", longitude: "2.3798866", category: "Autres", identity_card: "Y", driver_card: "N", passport: "Y", user: user_rayane })
+event_rayane_03 = Event.new({ title: "Demo day", venue: "Le Wagon", starts_at: DateTime.new(2022, 06, 10, 17, 00), address: "16 Vla Gaudelet, 75011 Paris", latitude: "48.8648601", longitude: "2.3798866", category: "Autres", identity_card: true, passport: true, user: user_rayane })
 event_rayane_03.document.attach(io: billet7, filename: "demo_day_3")
 event_rayane_03.save!
 
 #### patrick event creation
-event_patrick_01 = Event.new({ title: "Musee du Louvre", venue: "Musee du Louvre", starts_at: DateTime.new(2022, 06, 02, 10, 00), address: "Rue de Rivoli, 75001 Paris", latitude: "48.8607826", longitude: "2.3413157", category: "Culture", identity_card: "Y", driver_card: "N", passport: "Y", user: user_patrick })
+event_patrick_01 = Event.new({ title: "Musee du Louvre", venue: "Musee du Louvre", starts_at: DateTime.new(2022, 06, 02, 10, 00), address: "Rue de Rivoli, 75001 Paris", latitude: "48.8607826", longitude: "2.3413157", category: "Culture", identity_card: true, passport: true, user: user_patrick })
 event_patrick_01.document.attach(io: billet8, filename: "louvre")
 event_patrick_01.save!
