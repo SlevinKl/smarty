d34a52b2e8a91514bbc10aa8a312dac8325497b6
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-05-31 11:35:10 +0200

    validations done - reviewed with Cecile

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  CATEGORIES = %w[Sport Culture Voyage Autres]
   belongs_to :user
   validates :title, :venue, :starts_at, :address, :category, :latitude, :longitude, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
 end
