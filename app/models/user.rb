1329efff9d439209aa410174692d5add5de00bab
Rayane Moula <moula.rayane@gmail.com>
2022-05-31 11:53:30 +0200

    active storage setup, cloudinary avatar user,cloudinary document event

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :events, dependent: :destroy
+  has_one_attached :avatar
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :first_name, presence: true
