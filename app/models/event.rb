251eadd17a8c8afe0239c1778a1348759fda6c34
Rayane Moula <moula.rayane@gmail.com>
2022-06-07 17:29:53 +0200

    all validation is done

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,19 @@
 class Event < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-
+  before_validation :set_category
   CATEGORIES = %w[Sport Culture Voyage Autres]
 
   has_one_attached :document
   belongs_to :user
-  # validates :title, :venue, :starts_at, :address, :category, presence: true
-  # validates :category, inclusion: { in: CATEGORIES }
+  validates :title, :venue, :starts_at, :address, :category, presence: true, if: -> { status == "after_ocr" }
+  validates :category, inclusion: { in: CATEGORIES }, if: -> { status == "after_ocr" }
+
+  private
+
+  def set_category
+    return if category.present?
+
+    self.category = "Autres"
+  end
 end
