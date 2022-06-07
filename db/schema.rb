5bbaaab16c9c3a7dd51fc7e0ca9156948889be5f
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-06-07 09:51:43 +0200

    added admin to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_31_093812) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_07_074820) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
