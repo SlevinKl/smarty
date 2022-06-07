e0c2acadd660a68b466c3548e86f147c4ae2a7cd
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-06-07 09:55:01 +0200

    added admin to users

--- db/schema.rb
+++ db/schema.rb
@@ -70,6 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_07_074820) do
     t.string "first_name"
     t.string "last_name"
     t.string "phone"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
